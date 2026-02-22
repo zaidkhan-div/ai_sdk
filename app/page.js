d09d3939bcd9b745f26fd19382cf9ccb825d7274
zaidkhan-div <zaidscestudent@gmail.com>
2026-02-22 15:55:18 +0500

    initialzed the repo

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/ui/Hero'
+import Chat from './components/ui/chat'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Chat />
+    </>
+  )
+}
+
+export default Home
